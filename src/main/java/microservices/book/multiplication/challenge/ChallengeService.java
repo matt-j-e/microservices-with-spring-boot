b691a71724146c20d3ff7bb10b0d5db4872e54ac
mattedwards <the.matt.edwards@googlemail.com>
2022-02-13 16:07:18 +0000

    Add repositories to connect to Entities. Connect repositories to Service layer. Build tests.

--- src/main/java/microservices/book/multiplication/challenge/ChallengeService.java
+++ src/main/java/microservices/book/multiplication/challenge/ChallengeService.java
@@ -1,5 +1,7 @@
 package microservices.book.multiplication.challenge;
 
+import java.util.List;
+
 public interface ChallengeService {
   /**
    * Verifies if an attempt coming from the presentation layer is correct or not
@@ -7,4 +9,13 @@ public interface ChallengeService {
    * @return the resulting ChallengeAttempt object
    */
   ChallengeAttempt verifyAttempt(ChallengeAttemptDTO resultAttempt);
+
+  /**
+   * Gets latest attempts for a given user
+   *
+   * @param userAlias the user's alias
+   * @return a list of the last 10 {@link ChallengeAttempt} objects
+   * created by the user.
+   */
+  List<ChallengeAttempt> getLatestAttemptsForUser(String userAlias);
 }
